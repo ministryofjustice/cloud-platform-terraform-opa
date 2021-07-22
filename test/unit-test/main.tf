502101586388cd156c60d5a895aa891c030806b2
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-07-22 14:44:01 +0100

    fixing the test

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -3,13 +3,29 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "eu-west-2"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    iam     = "http://localhost:4566"
+    route53 = "http://localhost:4566"
+    sts     = "http://localhost:4566"
+  }
 }
 
 module "cert_manager" {
-  source              = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.2.1"
-  cluster_domain_name = "opa.cloud-platform.service.justice.gov.uk"
-  eks                 = false
+  source                = "github.com/ministryofjustice/cloud-platform-terraform-certmanager?ref=1.2.1"
+  cluster_domain_name   = "opa.cloud-platform.service.justice.gov.uk"
+  eks                   = false
+  dependence_prometheus = "ignore"
+  dependence_opa        = "ignore"
+  iam_role_nodes        = "arn:aws:iam::000000000000:role/dummy"
+  hostzone              = "*"
 }
 
 module "opa" {
